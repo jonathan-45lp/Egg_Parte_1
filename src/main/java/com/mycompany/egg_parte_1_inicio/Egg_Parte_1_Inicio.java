c15a4bcb4b4e2623e70e3dd79bf6ef00d39e80d4
Jonathan <jonathanemma41@gmail.com>
2023-04-11 18:09:14 -0300

    primer commit

---
+++ src/main/java/com/mycompany/egg_parte_1_inicio/Egg_Parte_1_Inicio.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.egg_parte_1_inicio;
+
+/**
+ *
+ * @author jonat
+ */
+public class Egg_Parte_1_Inicio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
